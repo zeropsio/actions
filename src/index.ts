ea93ec1150a7ccc688c1bf89299b308dd3842907
Arjun Aditya <meow@nermalcat69.dev>
2024-09-17 11:55:31 +0530

    feat: setup caching for zcli

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
+import * as cache from '@actions/cache'
 
 async function run() {
   try {
     const serviceId = core.getInput('service-id', { required: true })
     const accessToken = core.getInput('access-token', { required: true })
 
-    core.info('Installing Zerops CLI...')
-    await exec.exec('curl', [
-      '-L',
-      'https://github.com/zeropsio/zcli/releases/latest/download/zcli-linux-amd64',
-      '-o',
-      '/usr/local/bin/zcli'
-    ])
-    await exec.exec('chmod', ['+x', '/usr/local/bin/zcli'])
+    const zcliPath = "/usr/local/bin/zcli"
+    const zcliCacheKey = "zcli-linux-amd64-cache"
+
+    const cacheKey = await cache.restoreCache([zcliPath], zcliCacheKey)
+    if (cacheKey) {
+      core.info('Zerops CLI cache hit')
+    } else {
+      core.info('Zerops CLI cache miss')
+
+      core.info('Installing Zerops CLI...')
+      await exec.exec('curl', [
+        '-L',
+        'https://github.com/zeropsio/zcli/releases/latest/download/zcli-linux-amd64',
+        '-o',
+        zcliPath
+      ])
+      await exec.exec('chmod', ['+x', zcliPath])
+
+      await cache.saveCache([zcliPath], zcliCacheKey)
+    }
 
     core.exportVariable('ZEROPS_TOKEN', accessToken)
 
