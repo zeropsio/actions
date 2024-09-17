a2ce58e21c681492a1b5a3c78d3347cbe8a2c5fa
Arjun Aditya <meow@nermalcat69.dev>
2024-09-17 11:49:06 +0530

    iterations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
-/**
- * The entrypoint for the action.
- */
-import { run } from './main'
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
+async function run() {
+  try {
+    const serviceId = core.getInput('service-id', { required: true })
+    const accessToken = core.getInput('access-token', { required: true })
+
+    core.info('Installing Zerops CLI...')
+    await exec.exec('curl', [
+      '-L',
+      'https://github.com/zeropsio/zcli/releases/latest/download/zcli-linux-amd64',
+      '-o',
+      '/usr/local/bin/zcli'
+    ])
+    await exec.exec('chmod', ['+x', '/usr/local/bin/zcli'])
+
+    core.exportVariable('ZEROPS_TOKEN', accessToken)
+
+    core.info('Logging in with Zerops token...')
+    await exec.exec(`zcli login ${accessToken}`)
+
+    const deployCommand = `zcli push --serviceId ${serviceId}`
+    core.info(`Executing: ${deployCommand}`)
+    await exec.exec(deployCommand)
+
+    core.info('Deployment completed successfully.')
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(`Action failed with error: ${error.message}`)
+    } else {
+      core.setFailed('Action failed with an unknown error.')
+    }
+  }
+}
+
 run()
